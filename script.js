0c65f6d9171b3383ad482d6acdc0e059a627ce95
Jorge Martinez <jorge1austin1@gmail.com>
2025-06-22 18:15:15 -0500

    JS reorganized

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+// VARIABLE & FUNCTION TO CONTROL BALL
 let ball = document.getElementById('float-circle');
 
-// FUNCTIONS THAT HAVE EVENT
+// functions with events
 function up() {
   ball.style.bottom = '250px';
 };
@@ -9,9 +9,26 @@ function up() {
 function down() {
   ball.style.bottom = '50px';
 };
+     
 
-// EVENT HANDLER
+// Event handlers to add movement
 document.addEventListener('keydown', up);
 document.addEventListener('keyup', down);
 
-     
+
+// VARIABLE & FUNCTION TO HIDE BUTTON
+let startGame = document.getElementById('button');
+
+startGame.addEventListener('click', function(){
+  startGame.style.display = 'none';
+});
+
+
+// FUNCTION TO MOVE FROM RIGHT TO LEFT PIPE
+let pippy = document.getElementById('pipe');
+
+function screenLeft() {
+  pippy.style.right = '10%';
+};
+
+
